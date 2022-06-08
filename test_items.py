c7dfb2f6f3d8bb50fb31ee62468c47d5df8563f6
andreevks <105436541+andreevks@users.noreply.github.com>
2022-06-08 15:46:28 +0300

    Исправлена ошибка с ассерт

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_user_should_see_button_add_basket(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), \
+    assert browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket"), \
         'Кнопка добавления в корзину не найдена'
     #sleep(15)
     # btn_title = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").text
